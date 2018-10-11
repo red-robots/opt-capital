af217eb16c36282f7c6a08bd5d55bd93a9edc11d
lisadebona <annalisa.quiamco@gmail.com>
2018-10-12 01:09:31 +0800

    Update (Oct 11, 2018)

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,23 +12,30 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     
     <?php 
+        if(!is_single()) {
             $str = get_the_title();
             $count = str_word_count($str);
             $title = $str;
             if($count>1) {
                 $pieces = explode(' ', $str);
+                $total_words = count($pieces);
                 $last_word = end($pieces);
                 $title = '';
                 $i=1; foreach($pieces as $word) {
                     $comma = ($i>1) ? ' ':'';
-                    if($i==$count) {
+                    if($i==$total_words) {
                         $title .= $comma . '<span class="last-word">'.$word.'</span>';
                     } else {
                         $title .= $comma . $word;
                     }
                     $i++;
                 }
+                
             }
+        } else {
+            $title = get_the_title();
+        }
+            
     ?>
 	<header class="entry-header">
         <h1 class="entry-title styledTitle"><?php echo $title; ?></h1>
