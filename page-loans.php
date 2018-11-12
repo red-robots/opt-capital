f4e0a0fb72e78e3b20a9d46a886700489ad7b6cc
Austin Crane <austincrane@gmail.com>
2018-11-12 13:09:37 -0500

    boxes per page

--- page-loans.php
+++ page-loans.php
@@ -27,7 +27,50 @@ get_header(); ?>
 			
 		</main><!-- #main -->
 	</div><!-- #primary -->
-<?php get_template_part('inc/boxes'); ?>
+<?php 
+$box1title = get_field('box_1_title');
+$box2title = get_field('box_2_title');
+$box3title = get_field('box_3_title');
+
+$box1desc = get_field('box_1_description');
+$box1link = get_field('box_1_link');
+
+$box2desc = get_field('box_2_description');
+$box2link = get_field('box_2_link');
+
+$box3desc = get_field('box_3_description');
+$box3link = get_field('box_3_link');
+ ?>
+ <section class="bott-page">
+	
+
+	<section class="boxes">
+		<div class="box box1">
+			<h2><?php echo $box1title; ?></h2>
+			<div class="desc"><?php echo $box1desc; ?></div>
+			<div class="learnmore swipe">
+				<div class='insider'></div>
+				<a href="<?php echo $box1link['url']; ?>">Learn More</a>
+			</div>
+		</div>
+		<div class="box box2">
+			<h2><?php echo $box2title; ?></h2>
+			<div class="desc"><?php echo $box2desc; ?></div>
+			<div class="learnmore swipe">
+				<div class='insider'></div>
+				<a href="<?php echo $box2link['url']; ?>">Learn More</a>
+			</div>
+		</div>
+		<div class="box box3">
+			<h2><?php echo $box3title; ?></h2>
+			<div class="desc"><?php echo $box3desc; ?></div>
+			<div class="learnmore swipe">
+				<div class='insider'></div>
+				<a href="<?php echo $box3link['url']; ?>">Learn More</a>
+			</div>
+		</div>
+	</section>
+</section>
 <?php
 get_footer();
 
